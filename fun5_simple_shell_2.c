19e8b95320be03fd204498b8f397bfdc8df58c33
omar-ahmed-hammad <omar.ahmed11269@gmail.com>
2023-10-06 02:01:31 -0700

    5. Simple shell 1.0

--- fun5_simple_shell_2.c
+++ fun5_simple_shell_2.c
@@ -16,3 +16,53 @@ void hanndle_env_cmd(void)
 	}
 	continue;
 }
+
+/**
+ * handle_error - function that handle NULL errors
+ * @error_message: print error message
+ *
+ * Return: error message
+ */
+
+void handle_error(char *error_message)
+{
+	char cmd;
+
+	perror(error_message);
+	free(cmd);
+	exit(EXIT_FAILURE);
+}
+/**
+ * get_path - function that handle env variable.
+ * @cmd: CLI iput or command
+ *
+ * Return: 0.
+ */
+
+char *get_path(char *cmd)
+{
+	char *path_env = getenv("PATH");
+
+	if (path_env == NULL)
+		return (NULL);
+	char *path = strtok(path_env, ":");
+
+	while (path != NULL)
+	{
+		char *full_path = malloc(strlen(path) + strlen(cmd) + 2);
+
+		if (full_path == NULL)
+		{
+			printf("malloc");
+			exit(EXIT_FAILURE);
+		}
+		sprintf(full_path, "%s/%s", path, cmd);
+
+		if (access(full_path, X_OK) == 0)
+			return (full_path);
+
+		free(full_path);
+		path = strtok(NULL, ":");
+	}
+	return (NULL);
+}
